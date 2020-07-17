8ce575ec52149143a7ea9362e45bc37baa162144
Benjamin Bernstein <bernsteinbr@gmail.com>
2020-07-16 22:29:29 -0400

    Fixes install script and altair

--- miniconda.sh
+++ miniconda.sh
@@ -1,19 +1,22 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    OS='Linux'
-    SHELL_NAME='bash'
+    OS="Linux"
+    SHELL_NAME="bash"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    OS='MacOSX'
-    SHELL_NAME='zsh'
+    OS="MacOSX"
+    SHELL_NAME="zsh"
 fi
 
-curl -o ~/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-${OS}-x86_64.sh
-# shasum -a 256 ~/miniconda.sh
+curl -o ${HOME}/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-${OS}-x86_64.sh
+shasum -a 256 ${HOME}/miniconda.sh
 CONDA_PATH="${HOME}/miniconda3"
-bash ~/miniconda.sh -b -p ${CONDA_PATH}
+bash ${HOME}/miniconda.sh -b -p ${CONDA_PATH}
+rm ${HOME}/miniconda.sh
 eval "$(${CONDA_PATH}/bin/conda shell.${SHELL_NAME} hook)"
-conda init
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    conda init
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    conda init zsh
+fi
 conda config --set auto_activate_base false
-source "~/.${SHELL_NAME}rc"
 conda env create -f environment.yml
 # pip list --format=freeze > conda_requirements.txt
-rm ~/miniconda.sh
